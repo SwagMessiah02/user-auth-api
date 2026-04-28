f97074e57c2b0ee74b1bfba67e4be548b4606f1c
SwagMessiah02 <vmdemesquita@gmail.com>
2026-04-27 21:51:20 -0300

    feature: Autenticação por token armazenado no cookie

--- src/main/java/com/example/user_auth/security/JwtSecurityFilter.java
+++ src/main/java/com/example/user_auth/security/JwtSecurityFilter.java
@@ -1,22 +1,19 @@
 package com.example.user_auth.security;
 
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 import org.springframework.web.filter.OncePerRequestFilter;
-
 import java.io.IOException;
-import java.util.Optional;
 
 @Component
 public class JwtSecurityFilter extends OncePerRequestFilter {
@@ -34,12 +31,17 @@ public class JwtSecurityFilter extends OncePerRequestFilter {
                                     HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
 
-        String header = request.getHeader("Authorization"); // Authorization: Bearer <token>
+        String token = null;
 
+        if (request.getCookies() != null) {
+            for (Cookie cookie : request.getCookies()) {
+                if ("authToken".equals(cookie.getName())) {
+                    token = cookie.getValue();
+                }
+            }
+        }
 
-        if (header != null) {
-            String token = header.substring(7);
-
+        try {
             if (token != null && jwtTokenService.isTokenValid(token)) {
                 String username = jwtTokenService.extractUsername(token);
 
@@ -52,8 +54,15 @@ public class JwtSecurityFilter extends OncePerRequestFilter {
 
                 SecurityContextHolder.getContext().setAuthentication(authentication);
             }
-        }
 
-        filterChain.doFilter(request, response);
+            filterChain.doFilter(request, response);
+
+        } catch (ExpiredJwtException e) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.setContentType("application/json;charset=UTF-8");
+        } catch (JwtException e) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.setContentType("application/json;charset=UTF-8");
+        }
     }
 }
