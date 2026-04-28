d95803477f9f2e1e856bb16306606ba1e176b235
SwagMessiah02 <vmdemesquita@gmail.com>
2026-04-27 21:54:43 -0300

    Configurando a aplicação para rodar no Tomcat

--- src/main/java/com/example/user_auth/UserAuthApplication.java
+++ src/main/java/com/example/user_auth/UserAuthApplication.java
@@ -2,9 +2,16 @@ package com.example.user_auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class UserAuthApplication {
+public class UserAuthApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(UserAuthApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserAuthApplication.class, args);
