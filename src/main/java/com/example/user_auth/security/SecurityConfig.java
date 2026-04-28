ef8e64754b2798fd48d9df55363fe27eeffe8363
SwagMessiah02 <vmdemesquita@gmail.com>
2026-04-27 21:53:41 -0300

    refactor: Configurando as rotas de acesso a aplicação

--- src/main/java/com/example/user_auth/security/SecurityConfig.java
+++ src/main/java/com/example/user_auth/security/SecurityConfig.java
@@ -4,6 +4,8 @@ import com.example.user_auth.models.Role;
 import com.example.user_auth.repository.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.ProviderManager;
@@ -16,7 +18,6 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.access.intercept.AuthorizationFilter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
@@ -36,17 +37,42 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(CsrfConfigurer::disable)
+//                .cors((configure) -> configure.configurationSource(corsConfigurationSource()))
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(requests -> requests
                         .requestMatchers("/auth/login", "/auth/register")
                         .permitAll()
-                        .requestMatchers("/auth/admin")
-                        .hasAuthority(Role.Values.ADMIN.role)
-                        .requestMatchers("/auth/protected")
-                        .authenticated()
+                        .requestMatchers("/user/login", "/user/register")
+                        .permitAll()
+                        .requestMatchers("/user/protected")
+                        .hasAuthority(Role.Values.BASIC.role)
+                        .requestMatchers("/**")
+                        .permitAll()
                         .anyRequest().authenticated())
                 .addFilterBefore(jwtSecurityFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+// Caso use um servidor frontend, descomente e faça os ajustes necessários
+//    @Bean
+//    public CorsConfigurationSource corsConfigurationSource() {
+//        CorsConfiguration config = new CorsConfiguration();
+//        config.setAllowedOrigins(List.of("http://localhost:3000"));
+//        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+//        config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+//        config.setAllowCredentials(true);
+//
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", config);
+//        return source;
+//    }
+
+    @Bean
+    public RoleHierarchy roleHierarchy() {
+        return RoleHierarchyImpl.withRolePrefix("")
+                .role(Role.Values.ADMIN.role)
+                .implies(Role.Values.BASIC.role)
                 .build();
     }
 
