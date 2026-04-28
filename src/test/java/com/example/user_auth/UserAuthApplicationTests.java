f1149f7fc2bd37cf0bc2e9e8b0e73b86def92b06
SwagMessiah02 <vmdemesquita@gmail.com>
2026-04-27 21:56:51 -0300

    Refactor

--- src/test/java/com/example/user_auth/UserAuthApplicationTests.java
+++ src/test/java/com/example/user_auth/UserAuthApplicationTests.java
@@ -1,13 +1,20 @@
 package com.example.user_auth;
 
+import com.example.user_auth.controllers.AuthController;
+import com.example.user_auth.dtos.LoginDTO;
+import com.example.user_auth.dtos.RegisterDTO;
+import com.example.user_auth.exceptions.InvalidRequestException;
+import com.example.user_auth.repository.RoleRepository;
+import com.example.user_auth.repository.UserRepository;
+import com.example.user_auth.security.JwtTokenService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootTest
 class UserAuthApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
